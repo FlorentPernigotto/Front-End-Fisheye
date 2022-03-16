cd6adfdb375e9c6a9471e291febebe8baf4ff46d
FlorentPernigotto <92726803+FlorentPernigotto@users.noreply.github.com>
2022-03-16 15:28:17 +0100

    photographer.js
    ajout de contenue a la carte du profil du photographe

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,41 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait , city , country, tagline, price} = data;
 
     const picture = `assets/photographers/portrait/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement( 'article' ); // création de la card
+        const img = document.createElement( 'img' ); // insertion de l'image profil
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+        const h2 = document.createElement( 'h2' ); // création du nom de profil
         h2.textContent = name;
+        const location = document.createElement( 'div' );
+        location.setAttribute("class", location);
+        const cityValue = document.createElement('span');  
+        cityValue.textContent = city;
+        const virgule = document.createElement( 'span' )
+        virgule.textContent = ",";
+        const countryValue = document.createElement('span');
+        countryValue.textContent = country;
+        const quoteValue = document.createElement('quote');
+        quoteValue.textContent = tagline;
+        const priceContainer = document.createElement('div');
+        const priceValue = document.createElement('span');
+        priceValue.textContent = price;
+        const valuePerDay = document.createElement('span');
+        valuePerDay.textContent = "€/jours" ;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        location.appendChild(cityValue);
+        location.appendChild(virgule);
+        location.appendChild(countryValue);
+        article.appendChild(quoteValue);
+        article.appendChild(priceContainer);
+        priceContainer.appendChild(priceValue);
+        priceContainer.appendChild(valuePerDay)
         return (article);
     }
     return { name, picture, getUserCardDOM }
