843afb1ab92dde06e96636dd2785e06691c92631
FlorentPernigotto <92726803+FlorentPernigotto@users.noreply.github.com>
2022-03-17 12:02:10 +0100

    index
    mise en place des card-cliquable

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -5,13 +5,15 @@ function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' ); // création de la card
+        const a = document.createElement( 'a' );
+        a.setAttribute("href", "photographer.html")
         const img = document.createElement( 'img' ); // insertion de l'image profil
         img.setAttribute("src", picture);
         img.setAttribute("alt", name);
         const h2 = document.createElement( 'h2' ); // création du nom de profil
         h2.textContent = name;
         const location = document.createElement( 'div' );
-        location.setAttribute("class", location);
+        location.setAttribute("class", "location");
         const cityValue = document.createElement('span');  
         cityValue.textContent = city;
         const virgule = document.createElement( 'span' )
@@ -26,14 +28,15 @@ function photographerFactory(data) {
         const valuePerDay = document.createElement('span');
         valuePerDay.textContent = "€/jours" ;
 
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(location);
+        article.appendChild(a);
+        a.appendChild(img);
+        a.appendChild(h2);
+        a.appendChild(location);
         location.appendChild(cityValue);
         location.appendChild(virgule);
         location.appendChild(countryValue);
-        article.appendChild(quoteValue);
-        article.appendChild(priceContainer);
+        a.appendChild(quoteValue);
+        a.appendChild(priceContainer);
         priceContainer.appendChild(priceValue);
         priceContainer.appendChild(valuePerDay)
         return (article);
