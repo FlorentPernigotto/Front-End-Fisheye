301a2a637206e18167048d4627f82876673cf49b
FlorentPernigotto <92726803+FlorentPernigotto@users.noreply.github.com>
2022-03-23 17:36:59 +0100

    .

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,12 +1,12 @@
 function photographerFactory(data) {
-    const { name, portrait , city , country, tagline, price} = data;
+    const { name, portrait , city , country, tagline, price, id} = data;
 
     const picture = `assets/photographers/portrait/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' ); // création de la card
         const a = document.createElement( 'a' );
-        a.setAttribute("href", "photographer.html")
+        a.setAttribute("href", `photographer.html?id=${id}`);
         const img = document.createElement( 'img' ); // insertion de l'image profil
         img.setAttribute("src", picture);
         img.setAttribute("alt", name);
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,52 @@
-//code JavaScript lié à la page photographer.html
+document.addEventListener("DOMContentLoaded", ()=>{
+     let photographerId = window.location.search;
+
+         photographerId = new URLSearchParams(photographerId);
+
+         photographerId = photographerId.get("id");
+
+         fetch("data/photographers.json").then((response)=>{
+
+                return response.json();
+
+         }).then((values)=>{
+
+            const photographersInfos = values.photographers;
+
+            const photographersMedias = values.media;
+
+            displayPhotographerInfo(photographersInfos);
+
+            displayPhotographerMedias(photographersMedias);
+            
+                
+         }).catch((error)=>{
+
+             console.log(error);
+         });
+
+
+         function displayPhotographerInfo(photographersArray){
+
+              const photographerInfo = photographersArray.filter((photographer)=>{
+
+                    return photographer.id === Number(photographerId);
+
+              });
+
+              
+
+         }
+
+         // filtre tout les médias en fonction de l'id photograph
+
+         function displayPhotographerMedias(mediasArray){
+
+            const photographersMedias = mediasArray.filter((media)=>{
+
+                return media.photographerId === Number(photographerId);
+            });
+
+            console.log (photographerId)
+         }
+});
