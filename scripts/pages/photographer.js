2bef543e0224f23c1d7f641b9de48625fb5a2278
FlorentPernigotto <92726803+FlorentPernigotto@users.noreply.github.com>
2022-04-13 13:37:56 +0200

    fix update
    lecteur video fixed
    début de la création de la boite like-expand

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -56,7 +56,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
             const photographersMedias = mediasArray.filter((media)=>{
                 return media.photographerId === Number(photographerId);
               });
-            console.log(photographerId);
             
             const photographerPicture = `
             
@@ -71,7 +70,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
                   <div class="card-picture">
                     <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[2].image}" alt="${photographersMedias[2].title}le ${photographersMedias[2].date}">
                     <div class="under-picture">
-                      <span class="picture-name">${photographersMedias[2].title}</span><span class="picture-likes">${photographersMedias[2].likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></i></span>
+                      <span class="picture-name">${photographersMedias[2].title}</span><span class="picture-likes">${photographersMedias[2].likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></span>
                     </div>
                   </div>
                   <div class="card-picture">
@@ -97,8 +96,42 @@ document.addEventListener("DOMContentLoaded", ()=>{
                     <div class="under-picture">
                       <span class="picture-name">${photographersMedias[6].title}</span><span class="picture-likes">${photographersMedias[6].likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></span>
                     </div>
+                  </div>
+                  <div class="card-picture">
+                    <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[7].image}" alt="${photographersMedias[7].title}le ${photographersMedias[7].date}">
+                    <div class="under-picture">
+                      <span class="picture-name">${photographersMedias[7].title}</span><span class="picture-likes">${photographersMedias[7].likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></span>
+                    </div>
+                  </div> 
+                  <div class="card-picture">
+                    <video controls>
+                      <source src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[8].video}" type="video/mp4" alt="${photographersMedias[8].title}le ${photographersMedias[8].date}">
+                      Vôtre navigateur ne prend pas en charge cette vidéo.
+                    </video>
+                      <div class="under-picture">
+                      <span class="picture-name">${photographersMedias[8].title}</span><span class="picture-likes">${photographersMedias[8].likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></span>
+                    </div>
+                  </div> 
+                  <div class="card-picture">
+                    <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[9].image}" alt="${photographersMedias[9].title}le ${photographersMedias[9].date}">
+                    <div class="under-picture">
+                      <span class="picture-name">${photographersMedias[9].title}</span><span class="picture-likes">${photographersMedias[9].likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></span>
+                    </div>
+                  </div> 
+                  <div class="card-picture">
+                  <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[0].image}" alt="${photographersMedias[0].title}le ${photographersMedias[0].date}">
+                    <div class="under-picture">
+                      <span class="picture-name">${photographersMedias[0].title}</span><span class="picture-likes">${photographersMedias[0].likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></span>
+                    </div>
                   </div> 
                 </div>
+              </div>
+              <div class="photographer-like-expand">
+                <span class="all-media-likes"> 297 081 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg>
+                </span>
+                <span class="average-per-day">
+              
+                </span>
               </div>
             `;
 
@@ -106,8 +139,19 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
           }
 
+          function displayPhotographerExpand(mediasArray){
+            const photographersPrice = photographersArray.filter((photographer)=>{
+          
+              return photographer.price === Number(photographersPrice);
+          
+            }); 
+            console.log(photographersPrice);
+            const photographerLikeExpand = `
+              
+              `;
+          
+            document.querySelector(".photographer-like-expand").innerHTML = photographerLikeExpand;
+          
+          }
 });
 
-function displayInfoContainer(){
-
-}
