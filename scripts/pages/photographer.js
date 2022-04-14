89faca16b805ad5d8c249242c18769d9e547eb4e
FlorentPernigotto <92726803+FlorentPernigotto@users.noreply.github.com>
2022-04-14 13:06:51 +0200

    .

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -48,6 +48,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
             document.querySelector(".photograph-header").innerHTML = photographerProfile;
 
+            const photographerPrice = `
+
+            <span>${photographersInfos[0].price}</span>
+            `;
+
+            document.querySelector(".average-per-day").innerHTML = photographerPrice;
           }
 
          // filtre tout les médias en lié de l'id photograph
@@ -56,7 +62,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
             const photographersMedias = mediasArray.filter((media)=>{
                 return media.photographerId === Number(photographerId);
               });
-            
+
             const photographerPicture = `
             
               <div class="card-container">
@@ -104,7 +110,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
                     </div>
                   </div> 
                   <div class="card-picture">
-                    <video controls>
+                    <video>
                       <source src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[8].video}" type="video/mp4" alt="${photographersMedias[8].title}le ${photographersMedias[8].date}">
                       Vôtre navigateur ne prend pas en charge cette vidéo.
                     </video>
@@ -126,32 +132,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
                   </div> 
                 </div>
               </div>
-              <div class="photographer-like-expand">
-                <span class="all-media-likes"> 297 081 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg>
-                </span>
-                <span class="average-per-day">
-              
-                </span>
-              </div>
             `;
 
             document.querySelector(".photographer-picture").innerHTML = photographerPicture;
 
-          }
-
-          function displayPhotographerExpand(mediasArray){
-            const photographersPrice = photographersArray.filter((photographer)=>{
-          
-              return photographer.price === Number(photographersPrice);
-          
-            }); 
-            console.log(photographersPrice);
-            const photographerLikeExpand = `
-              
-              `;
-          
-            document.querySelector(".photographer-like-expand").innerHTML = photographerLikeExpand;
-          
           }
 });
 
