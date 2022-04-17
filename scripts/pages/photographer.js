2b1cc85bff25e5668792a56d25c11986faa91215
FlorentPernigotto <92726803+FlorentPernigotto@users.noreply.github.com>
2022-04-17 18:42:53 +0200

    gallery check

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -62,125 +62,51 @@ document.addEventListener("DOMContentLoaded", ()=>{
             const photographersMedias = mediasArray.filter((media)=>{
                 return media.photographerId === Number(photographerId);
               });
-<<<<<<< HEAD
-            const photographerPicture = `
+              
+              function mediasFactory(media){
             
-              <div class="card-container">
-                <div class="picture-container">
-                  <div class="card-picture">
-                    <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[1].image}" alt="${photographersMedias[1].title}le ${photographersMedias[1].date}">
-                    <div class="under-pîcture">
-                      <span class="picture-name">${photographersMedias[1].title}</span><span class="picture-likes">${photographersMedias[1].likes}</span>
-                    </div>
-                    </div>
-                  <div class="card-picture">
-                    <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[2].image}" alt="${photographersMedias[2].title}le ${photographersMedias[2].date}">
-                    <div class="under-pîcture">
-                      <span class="picture-name">${photographersMedias[2].title}</span><span class="picture-likes">${photographersMedias[2].likes}</span>
-                    </div>
-                    </div>
-                  <div class="card-picture">
-                    <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[3].image}" alt="${photographersMedias[3].title}le ${photographersMedias[3].date}">
-                    <div class="under-pîcture">
-                      <span class="picture-name">${photographersMedias[3].title}</span><span class="picture-likes">${photographersMedias[3].likes}</span>
-                    </div>
-                    </div>
-                  <div class="card-picture">
-                    <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[4].image}" alt="${photographersMedias[4].title}le ${photographersMedias[4].date}">
-                    <div class="under-pîcture">
-                      <span class="picture-name">${photographersMedias[4].title}</span><span class="picture-likes">${photographersMedias[4].likes}</span>
-                    </div>
-                    </div>
-                  <div class="card-picture">
-                    <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[5].image}" alt="${photographersMedias[5].title}le ${photographersMedias[5].date}">
-                    <div class="under-pîcture">
-                      <span class="picture-name">${photographersMedias[5].title}</span><span class="picture-likes">${photographersMedias[5].likes}</span>
-                    </div>
-                    </div>
-                  <div class="card-picture">
-                    <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[6].image}" alt="${photographersMedias[6].title}le ${photographersMedias[6].date}">
-                    <div class="under-pîcture">
-                      <span class="picture-name">${photographersMedias[6].title}</span><span class="picture-likes">${photographersMedias[6].likes}</span>
-                    </div>
-                  </div> 
-                  <div class="card-picture">
-                    <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[7].image}" alt="${photographersMedias[7].title}le ${photographersMedias[7].date}">
-                    <div class="under-pîcture">
-                      <span class="picture-name">${photographersMedias[7].title}</span><span class="picture-likes">${photographersMedias[7].likes}</span>
-                    </div>
-                  </div> 
-                  <div class="card-picture">
-                    <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[8].image}" alt="${photographersMedias[8].title}le ${photographersMedias[8].date}">
-                    <div class="under-pîcture">
-                      <span class="picture-name">${photographersMedias[8].title}</span><span class="picture-likes">${photographersMedias[8].likes}</span>
-                    </div>
-                  </div> 
-                  <div class="card-picture">
-                    <img src="assets/photographers/${photographersMedias[0].photographerId}/${photographersMedias[9].image}" alt="${photographersMedias[9].title}le ${photographersMedias[9].date}">
-                    <div class="under-pîcture">
-                      <span class="picture-name">${photographersMedias[9].title}</span><span class="picture-likes">${photographersMedias[9].likes}</span>
-                    </div>
-                  </div> 
+                if(media.image !== undefined){
+                
+                      return createImage(media);
+                
+                }
+                
+                return createVideo(media);
+          
+          }
+          
+          function createImage(sourceImg){
+          
+              return `
+              <div class="card-picture">
+                <img src="assets/photographers/${photographersMedias[0].photographerId}/${sourceImg.image}" alt="${sourceImg.title}le ${sourceImg.date}">
+                <div class="under-picture">
+                  <span class="picture-name">${sourceImg.title}</span><span class="picture-likes">${sourceImg.likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></span>
                 </div>
-              </div>
-            `;
-
-            document.querySelector(".photographer-picture").innerHTML = photographerPicture;
-=======
-
-            function mediasFactory(media){
-            
-                  if(media.image !== undefined){
-                  
-                        return createImage(media);
-                  
-                  }
-                  
-                  return createVideo(media);
-            
-            }
-            
-            function createImage(sourceImg){
-            
-                return `
-                <div class="card-picture">
-                    <img src="assets/photographers/${photographersMedias[0].photographerId}/${sourceImg.image}" alt="${photographersMedias[1].title}le ${photographersMedias[1].date}">
-                    <div class="under-picture">
-                      <span class="picture-name">${photographersMedias[1].title}</span><span class="picture-likes">${photographersMedias[1].likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></span>
-                    </div>
-                  </div>`;
-            }
-            
-            function createVideo(sourceVideo){
-            
-                return `<div class="card-picture">
-                <video alt=${sourceVideo.alt} controls>
-                  <source src=assets/photographers/${photographersMedias[0].photographerId}/${sourceVideo.video}>
+              </div>`;
+          }
+          
+          function createVideo(sourceVideo){
+          
+              return `
+              <div class="card-picture">
+                <video alt="${sourceVideo.alt}" controls>
+                  <source src="assets/photographers/${photographersMedias[0].photographerId}/${sourceVideo.video}" alt="${sourceVideo.title}le ${sourceVideo.date}">
                 </video>  
                 <div class="under-picture">
-                    <span class="picture-name">${photographersMedias[1].title}</span><span class="picture-likes">${photographersMedias[1].likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></span>
-                  </div>
-                </div>`
-            }
-            let mediasHTML = "";
-            photographersMedias.forEach((element)=>{
-                    mediasHTML+= `
-                    
-                        ${mediasFactory(element)}
-                    
-                    `;
-            
-            });
-            
-            document.querySelector(".picture-container").innerHTML = mediasHTML;
-            
-            
-            
->>>>>>> 1cbb6495b4724f49f170516724aa1907e778bd60
-
+                  <span class="picture-name">${sourceVideo.title}</span><span class="picture-likes">${sourceVideo.likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></span>
+                </div>
+              </div>`
           }
-});
-<<<<<<< HEAD
-=======
-
->>>>>>> 1cbb6495b4724f49f170516724aa1907e778bd60
+          let mediasHTML = "";
+          photographersMedias.forEach((element)=>{
+                  mediasHTML+= `
+                  
+                      ${mediasFactory(element)}
+                  
+                  `;
+          
+          });
+          
+          document.querySelector(".picture-container").innerHTML = mediasHTML;
+}});
