79f77c7da166016854e09bbf882ba545b40e994d
FlorentPernigotto <92726803+FlorentPernigotto@users.noreply.github.com>
2022-04-21 07:12:06 +0200

    .

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -63,8 +63,66 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 return media.photographerId === Number(photographerId);
               });
               
+              function lightbox(mediasArray, media){
+  
+                // Le media actuel qui est dans la lightbox
+           
+                let actualMediaId = media.id;
+           
+               //La lightbox est creee
+           
+           
+             const lighboxContent = document.querySelector('.picture-container');
+           
+           
+             lighboxContent.innerHTML = mediasFactory(media);
+           
+             const next = document.querySelector("#next");
+           
+             const previous = document.querySelector("#previous");
+           
+           
+           next.addEventListener("click", ()=>{
+             
+                 // Premierement il faut determinet l'index du media actuel dans la lightbox
+             
+                 const actualIndex = mediasArray.findIndex((element)=>{
+                   
+                     return element.id === actualMediaId;
+                   
+                 });
+             
+                 if(actualIndex !== mediasArray.length){
+                   
+                      lighboxContent.innerHTML = mediasFactory(mediasArray[actualIndex + 1]);
+                   
+                      return actualMediaId = mediasArray[actualIndex + 1].id;
+                   
+                 }
+             
+           });
+           
+           previous.addEventListener("click", ()=>{
+             
+                 const actualIndex = mediasArray.findIndex((element)=>{
+                   
+                     return element.id === actualMediaId;
+                   
+                 });
+             
+                if(actualIndex !== 0){
+                  
+                     lighboxContent.innerHTML = mediasFactory(medias[actualIndex - 1]);
+                   
+                      return actualMediaId = medias[actualIndex - 1].id;
+                   
+                  
+                }
+           });
+         }
+              
               function mediasFactory(media){
-            
+
                 if(media.image !== undefined){
                 
                       return createImage(media);
@@ -74,11 +132,13 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 return createVideo(media);
           
           }
+
           
           function createImage(sourceImg){
           
               return `
-              <div class="card-picture">
+              <div class="card-media">
+                <a href="assets/photographers/${photographersMedias[0].photographerId}/${sourceImg.image}" data-lightbox="image-1" data-title="${sourceImg.title}">
                 <img src="assets/photographers/${photographersMedias[0].photographerId}/${sourceImg.image}" alt="${sourceImg.title}le ${sourceImg.date}">
                 <div class="under-picture">
                   <span class="picture-name">${sourceImg.title}</span><span class="picture-likes">${sourceImg.likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></span>
@@ -87,12 +147,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
           }
           
           function createVideo(sourceVideo){
-          
               return `
-              <div class="card-picture">
-                <video alt="${sourceVideo.alt}" controls>
+              <div class="card-media">
+                <a href="assets/photographers/${photographersMedias[0].photographerId}/${sourceVideo.video}">
+                <video alt="${sourceVideo.alt}">
                   <source src="assets/photographers/${photographersMedias[0].photographerId}/${sourceVideo.video}" alt="${sourceVideo.title}le ${sourceVideo.date}">
-                </video>  
+                </video>
                 <div class="under-picture">
                   <span class="picture-name">${sourceVideo.title}</span><span class="picture-likes">${sourceVideo.likes}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="currentColor" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></span>
                 </div>
@@ -109,4 +169,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
           });
           
           document.querySelector(".picture-container").innerHTML = mediasHTML;
+
+          
 }});
